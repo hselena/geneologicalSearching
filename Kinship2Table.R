d0e90d73b3af54369588934a1a28487980136572
cmperez8 <48361472+cmperez8@users.noreply.github.com>
2019-05-05 14:45:40 -0700

    Add files via upload
    added while loop instead of for loop

--- Kinship2Table.R
+++ Kinship2Table.R
@@ -10,52 +10,65 @@ total = cbind(fam_anon, parentIDs)
 print("total matrix made")
 write.table(total, "~/Cynthia/total.txt", sep = "\t")
 
-children = unique(relation[,2])
+children = relation[,2]
 print(length(children))
-num = 0
 parentID = c()
 childID = c()
 NewID = total[nrow(total),1]
 count = 0
+num = 0
+i = 1
 time1 = Sys.time()
-for( i in 1:length(children)){
+while (i <= length(children)) {
   count = count + 1 
-  parents =  relation[which(relation[,2] == children[i]), 1]
-  if (length(parents) == 1){
-    NewID = NewID + 1 
-    parents = append(parents, NewID)
+  if(relation[i,2] == relation[i+1,2]){ #if this child has 2 parents, parents into vector, and child variable 
+    parents = c(relation[i,1], relation[i+1,1])
+    child = relation[i,2]
+    print(parents)
+    print(child)
+    i = i + 1 
+  } else{ # this child has one parent 
+    NewID = as.numeric(NewID) + 1 
+    parents = c(NewID, relation[i,1])
+    child = relation[i,2]
     newrow = c(NewID, "unknown", NA, NA)
     total = rbind(total, newrow)
-    rownames(total)[nrow(total)] = as.character(NewID)
+    print(parents)
+    print(child)
   } 
-  for ( x in parents){
+  for( x in parents){
     sex = total[as.character(x),2]
-    if( sex == "female"){
+    print(sex)
+    if(!is.na(sex) && sex == "female"){
       #momID 
-      total[as.character(children[i]), 4] = x
-    } else if (sex == "male") {
+      total[as.character(child), 4] = x
+    } else if (!is.na(sex) && sex == "male") {
       #dadID 
-      total[as.character(children[i]),3] = x 
+      total[as.character(child),3] = x 
     } else { 
       num = num + 1
       parentID[num] = x 
-      childID[num] = children[i]
+      childID[num] = child
     }
-    
-  }
-  if (count%%200 == 0){
+  } 
+  i = i +1 
+  if (count%%200 == 0){ # 
     time2 = Sys.time()
     print(time2 - time1)
     count = 0 
     print(paste("ID: ", children[i]))
     print(paste("parents: ", parents))
   }
-}
+ 
+}#end while  
+  
+
 
 print("on to non gendered parents")
 count = 0
+time1 = Sys.time()
 for (i in 1:length(childID)){
-  time1 = Sys.time()
+  count = count + 1 
   if (is.na(total[as.character(childID[i]), 4])){
     total[as.character(childID[i]), 4] = parentID[i]
     total[as.character(parentID[i]),2] = "female"

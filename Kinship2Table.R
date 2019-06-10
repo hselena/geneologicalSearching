4a31f1cce77c443eaf195507309b899cc3186de0
cmperez8 <48361472+cmperez8@users.noreply.github.com>
2019-06-10 00:45:55 -0700

    Add files via upload
    testing with smaller subset of data as.numeric 
    change count%%20 == 0 remove nrows in relation variable after

--- Kinship2Table.R
+++ Kinship2Table.R
@@ -1,34 +1,32 @@
 setwd("~/FamilialSearch/familinix")
-if(!require(kinship2)) install.packages("kinship2",repos = "http://cran.us.r-project.org")
 ID_gender <- read.csv(file = "IDandGender.txt", header = T, sep = " ", stringsAsFactors = FALSE)
- 
-relation = read.csv( file ="child_parent.txt", header = T, sep = "\t")
+print("IDandGender.txt done ")
+relation = read.csv( file ="child_parent.txt", header = T, sep = "\t", nrows = 25)
+print("child_parent.txt done")
 
-print("relations-anon.txt done")
 ID_gender[which(ID_gender[,2] == "*"), 2 ] = "unknown"
 parentIDs = matrix(-1, nrow = length(ID_gender[,1]), ncol = 2)
 kinshipformatDF = cbind(ID_gender, parentIDs)
 colnames(kinshipformatDF) <- c("ID","gender" ,"FatherID", "MotherID")
+print("kinshipformatDF created")
 
 #another matrix of empty rows for new IDs created 
- 
 newIDs = matrix(c(NA,"unknown", NA, NA), nrow = 1943863, ncol = 4, byrow = TRUE)
 newIDs[,1] = 86127513:88071375
 colnames(newIDs) <- c("ID","gender" ,"FatherID", "MotherID")
+print("1943863 newIDs created")
 
 #combine matrix for newIDs and kinshipformatDF 
 kinshipformatDF = rbind(kinshipformatDF, newIDs)
-
+print("kinshipformatDF rbind w/ newIDs")
 
 children = relation[,2]
-parentID = c()
-childID = c()
 newIDcounter = 86127512
-count = 0
-num = 0
 i = 1
 time1 = Sys.time()
-while (i <= (children)) { #length(children)
+count = 0
+
+while (i <= length(children)) { 
   count = count + 1 
   #if this child has 2 parents, parents into vector, and child variable 
   if(relation[i,2] == relation[i+1,2]){ 
@@ -40,35 +38,38 @@ while (i <= (children)) { #length(children)
   else{ 
     child = relation[i,2]
     otherParent = newIDcounter +1 
-    parents = c(otherParent, relation[i,1])
+    parents = c(relation[i,1], otherParent)
+    # only the sex of one parent is known 
+    if (kinshipformatDF[as.numeric(parents[1]),2] == "female"){
+      kinshipformatDF[as.numeric(parents[2]),2] = "male"
+      print(kinshipformatDF[as.numeric(parents[2]),2])
+    } else{ 
+      kinshipformatDF[as.numeric(parents[2]),2] = "female"
+      print(kinshipformatDF[as.numeric(parents[2]),2])
+    }
   } 
   #finding the sex of the parents
   for( x in parents){
-    sex = total[x,2]#total[as.character(x),2]
-    if(!is.na(sex) && sex == "female"){
-      #momID 
-      total[child,4] = x#total[as.character(child), 4] = x
-    } else if (!is.na(sex) && sex == "male") {
-      #dadID 
-      total[child,3] = x 
-    } else { 
-      #num = num + 1
-      parentID = c(parentID, x)#[num] = x 
-      childID = c(childID, child)#[num] = child
+    sex = kinshipformatDF[as.numeric(x),2]
+    print(sex)
+    if (sex == "female"){
+      # MOM ID 
+      kinshipformatDF[as.numeric(child),4] = x
+    } else{
+      # DAD ID
+      kinshipformatDF[as.numeric(child),3] = x
     }
   } 
   i = i +1 
-  if (count%%200 == 0){ # 
+  print(i)
+  if (count%%20 == 0){ # 
     time2 = Sys.time()
     print(time2 - time1)
     count = 0 
     print(paste("ID: ", children[i]))
     print(paste("parents: ", parents))
   }
- 
-}#end while  
-  
-
-
-write.table(total, "~/Cynthia/familinix_data.txt", sep = "\t")
+}
+#end while  
+write.table(kinshipformatDF, "~/Cynthia/kinshipformat.txt", sep = "\t")
 

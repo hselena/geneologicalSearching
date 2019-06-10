3a95ec76e5e7070e4b25e89a437aec5a47b619e8
cmperez8 <48361472+cmperez8@users.noreply.github.com>
2019-06-10 00:58:35 -0700

    Add files via upload

--- Kinship2Table.R
+++ Kinship2Table.R
@@ -27,7 +27,7 @@ time1 = Sys.time()
 count = 0
 
 while (i <= length(children)) { 
-  count = count + 1 
+  #count = count + 1 
   #if this child has 2 parents, parents into vector, and child variable 
   if(relation[i,2] == relation[i+1,2]){ 
     parents = c(relation[i,1], relation[i+1,1])
@@ -39,37 +39,45 @@ while (i <= length(children)) {
     child = relation[i,2]
     otherParent = newIDcounter +1 
     parents = c(relation[i,1], otherParent)
-    # only the sex of one parent is known 
+    # only the gender of one parent is known 
     if (kinshipformatDF[as.numeric(parents[1]),2] == "female"){
       kinshipformatDF[as.numeric(parents[2]),2] = "male"
+      print("first parent gender is female")
       print(kinshipformatDF[as.numeric(parents[2]),2])
     } else{ 
       kinshipformatDF[as.numeric(parents[2]),2] = "female"
+      print("first parent gender is male")
       print(kinshipformatDF[as.numeric(parents[2]),2])
     }
   } 
+  print(paste("the child is: ", child))
+  print(paste("parents are: ", parents))
   #finding the sex of the parents
   for( x in parents){
+    print("assinging them to either MotherID or FatherID")
     sex = kinshipformatDF[as.numeric(x),2]
     print(sex)
     if (sex == "female"){
       # MOM ID 
       kinshipformatDF[as.numeric(child),4] = x
+      print(paste("should be motherID", kinshipformatDF[as.numeric(child),4]))
     } else{
       # DAD ID
       kinshipformatDF[as.numeric(child),3] = x
+      print(paste("should be fatherID", kinshipformatDF[as.numeric(child),3]))
     }
   } 
   i = i +1 
-  print(i)
-  if (count%%20 == 0){ # 
-    time2 = Sys.time()
-    print(time2 - time1)
-    count = 0 
-    print(paste("ID: ", children[i]))
-    print(paste("parents: ", parents))
-  }
+  print(paste("i is: ", i))
 }
 #end while  
+
+if (count%%20 == 0){ # 
+  time2 = Sys.time()
+  print(time2 - time1)
+  count = 0 
+  print(paste("ID: ", children[i]))
+  print(paste("parents: ", parents))
+}
 write.table(kinshipformatDF, "~/Cynthia/kinshipformat.txt", sep = "\t")
 
